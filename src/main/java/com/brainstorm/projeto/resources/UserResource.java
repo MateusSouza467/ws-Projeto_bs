38fda7b4be8a4d6655da520585ae0fc2a3b04682
Mateus Souza <mateusjose1903@gmail.com>
2023-05-09 17:51:53 -0300

    User entity and resource

---
+++ src/main/java/com/brainstorm/projeto/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.brainstorm.projeto.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brainstorm.projeto.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria","maria@gmail.com","97684179","12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
