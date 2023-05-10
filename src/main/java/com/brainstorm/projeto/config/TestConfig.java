2d59188d0341dbf804f797b5c30615905196d5f1
Mateus Souza <mateusjose1903@gmail.com>
2023-05-10 13:25:51 -0300

    Payment,one-to-one association

--- src/main/java/com/brainstorm/projeto/config/TestConfig.java
+++ src/main/java/com/brainstorm/projeto/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.brainstorm.projeto.entities.Category;
 import com.brainstorm.projeto.entities.Order;
 import com.brainstorm.projeto.entities.OrderItem;
+import com.brainstorm.projeto.entities.Payment;
 import com.brainstorm.projeto.entities.Product;
 import com.brainstorm.projeto.entities.User;
 import com.brainstorm.projeto.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
