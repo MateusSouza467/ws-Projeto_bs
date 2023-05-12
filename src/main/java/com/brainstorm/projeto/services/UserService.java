e7990e59de7cbb29f9322e53b3764b9f2b27f472
Mateus Souza <mateusjose1903@gmail.com>
2023-05-12 11:43:16 -0300

    Exception handling - finById

--- src/main/java/com/brainstorm/projeto/services/UserService.java
+++ src/main/java/com/brainstorm/projeto/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.brainstorm.projeto.entities.User;
 import com.brainstorm.projeto.repositories.UserRepository;
+import com.brainstorm.projeto.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
